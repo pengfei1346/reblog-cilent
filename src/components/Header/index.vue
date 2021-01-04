ab1919502fd3eddd292ddec82adecf21f83e1620
cpf <chenpengfei@aicaihr.com>
2021-01-04 13:31:32 +0800

    modify

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,48 +1,67 @@
 <template>
-    <ul class="nav">
-        <router-link to="/index"><li :class="this.$route.path==='/index'?'active':''">首页</li></router-link>
-        <router-link to="/articles"><li :class="this.$route.path==='/articles'?'active':''">文章</li></router-link>
-        <!--<router-link to=""><li :class="this.$route.path===3?'active':''">分类</li></router-link>-->
-        <router-link to="/archive"><li :class="this.$route.path==='/archive'?'active':''">分类</li></router-link>
-        <router-link to="/collections"><li :class="this.$route.path==='/collections'?'active':''">收藏</li></router-link>
-        <router-link to="/timeLine"><li :class="this.$route.path==='/timeLine'?'active':''">时间轴</li></router-link>
-        <router-link to="/leavingMessage"><li :class="this.$route.path==='/leavingMessage'?'active':''">留言</li></router-link>
-        <router-link to="/about"><li :class="this.$route.path==='/about'?'active':''">关于</li></router-link>
-        <router-link to="/write"><li :class="this.$route.path==='/write'?'active':''">写文章</li></router-link>
-    </ul>
+  <ul class="nav">
+    <router-link to="/index">
+      <li :class="this.$route.path==='/index'?'active':''">首页</li>
+    </router-link>
+    <router-link to="/articles">
+      <li :class="this.$route.path==='/articles'?'active':''">文章</li>
+    </router-link>
+    <!--<router-link to=""><li :class="this.$route.path===3?'active':''">分类</li></router-link>-->
+    <router-link to="/archive">
+      <li :class="this.$route.path==='/archive'?'active':''">分类</li>
+    </router-link>
+    <router-link to="/collections">
+      <li :class="this.$route.path==='/collections'?'active':''">收藏</li>
+    </router-link>
+    <router-link to="/timeLine">
+      <li :class="this.$route.path==='/timeLine'?'active':''">时间轴</li>
+    </router-link>
+    <router-link to="/leavingMessage">
+      <li :class="this.$route.path==='/leavingMessage'?'active':''">留言</li>
+    </router-link>
+    <router-link to="/about">
+      <li :class="this.$route.path==='/about'?'active':''">关于</li>
+    </router-link>
+    <router-link to="/write">
+      <li :class="this.$route.path==='/write'?'active':''">写文章</li>
+    </router-link>
+  </ul>
 </template>
 
 <script>
-    export default {
-        name: "index"
-    }
+  export default {
+    name: "index"
+  }
 </script>
 
 <style scoped lang="scss">
-    .nav{
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 100;
-        background: #fff;
-        width: 100%;
-        line-height: 60px;
-        font-size: 16px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-shadow: 0 1px 40px -8px rgba(0, 0, 0, .5);
-        li {
-            color: #666 !important;
-            cursor: pointer;
-            margin: 0 14px;
-            &:hover{
-                color: rgb(230, 217, 113) !important;
-            }
-        }
-        .active{
-            color: rgb(230, 217, 113) !important;
-        }
+  .nav {
+    z-index: 998;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #fff;
+    width: 100%;
+    line-height: 60px;
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 1px 40px -8px rgba(0, 0, 0, .5);
+
+    li {
+      color: #666 !important;
+      cursor: pointer;
+      margin: 0 14px;
+
+      &:hover {
+        color: rgb(230, 217, 113) !important;
+      }
     }
+
+    .active {
+      color: rgb(230, 217, 113) !important;
+    }
+  }
 </style>
--- src/views/client/article/articleList.vue
+++ src/views/client/article/articleList.vue
@@ -1,79 +1,87 @@
 <template>
-    <div class="home">
-        <Header class="header"/>
+  <div class="home">
+    <Header class="header"/>
 
-        <div class="main">
-            <ul class="article-list">
-                <li v-for="(item,i) in list" :key="i" @click="handleToDetail(item._id)">
-                    <div class="article-top">
-                        <div class="feature-body">
-                            <img :src="item.coverImgUrl" class="feature-img">
-                        </div>
+    <div class="main">
+      <ul class="article-list">
+        <li v-for="(item,i) in list" :key="i" @click="handleToDetail(item._id)">
+          <div class="article-top">
+            <div class="feature-body">
+              <img :src="item.coverImgUrl" class="feature-img">
+            </div>
 
-                        <div class="article-body">
-                            <div class="top-title">
-                                <div>{{item.title}}</div>
-                                <div>发布于 {{item.createdAt | parseTime('{y}-{m}-{d}')}}</div>
-                            </div>
-                            <p>
-                                {{item.abstract}}
-                                <span>...</span>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="article-foot">...</div>
-                    <hr>
-                </li>
-            </ul>
-        </div>
+            <div class="article-body">
+              <div class="top-title">
+                <div>{{item.title}}</div>
+                <div>发布于 {{item.createdAt | parseTime('{y}-{m}-{d}')}}</div>
+              </div>
+              <p>
+                {{item.abstract}}
+                <span>...</span>
+              </p>
+
+              <div class="item-bottom">
+                <div class="detail-meta">
+                  <div class="meta-box"><i class="iconfont icon-yanjing"></i>{{item.browseNum}}</div>
+                  <div class="meta-box"><i class="iconfont icon-huifu"></i>{{item.replyNum}}</div>
+                  <div class="meta-box"><i class="iconfont icon-zuozhe"></i>{{item.author}}</div>
+                </div>
+                <div class="item-foot">...</div>
+              </div>
+            </div>
+          </div>
+          <hr>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from '@/components/Header'
+  import Header from '@/components/Header'
 
-    export default {
-        name: 'index',
-        components:{
-            Header
-        },
-        data () {
-            return{
-                // publishTime: '2018-12-3 4:21:11',
-                // title: '啊啊',
-                // heatNum: 33,
-                // commentNum: 56,
-                // author: '方法',
-                // abstract: '反反复复反反复复反反复复反反复复反反复复发发发',
-                // coverImgUrl: require('../assets/item-1.png')
-                list: [],
-                total: 0
-            }
-        },
-        created() {
-            this.getArticleList()
-        },
-        methods: {
-            headerTopDown() {
-                console.log(this.$refs.main.offsetTop);
-                window.scroll(0,this.$refs.main.offsetTop)
-                // let endScroll = document.getElementById('main').offset().top;
-            },
-            async getArticleList() {
-                const data = await this.$xhr.get('/article')
-                this.list = data.data
-                this.total = data.total
-            },
-            handleToDetail(id) {
-                if(!id) return
-                this.$router.push({
-                    path: `/articles/${id}`
-                })
-            }
-        }
+  export default {
+    name: 'index',
+    components: {
+      Header
+    },
+    data() {
+      return {
+        // publishTime: '2018-12-3 4:21:11',
+        // title: '啊啊',
+        // heatNum: 33,
+        // commentNum: 56,
+        // author: '方法',
+        // abstract: '反反复复反反复复反反复复反反复复反反复复发发发',
+        // coverImgUrl: require('../assets/item-1.png')
+        list: [],
+        total: 0
+      }
+    },
+    created() {
+      this.getArticleList()
+    },
+    methods: {
+      headerTopDown() {
+        console.log(this.$refs.main.offsetTop);
+        window.scroll(0, this.$refs.main.offsetTop)
+        // let endScroll = document.getElementById('main').offset().top;
+      },
+      async getArticleList() {
+        const data = await this.$xhr.get('/article')
+        this.list = data.data
+        this.total = data.total
+      },
+      handleToDetail(id) {
+        if (!id) return
+        this.$router.push({
+          path: `/articles/${id}`
+        })
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
- @import "style/articleList";
+  @import "style/articleList";
 </style>
